a2923f42a09ab6aa12b9bcb7f73ffebc20377cfa
arri1 <ewws@mail.ru>
2023-09-21 13:32:20 +0300

    lab 1

--- gerasimovGeorgiy/App.js
+++ gerasimovGeorgiy/App.js
@@ -1,20 +1,33 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, TouchableOpacity, View } from "react-native";
+import { useState } from "react";
 
 export default function App() {
+  const [count, setCount] = useState(0);
   return (
-    <View style={styles.container}>
-      <Text>Hello world</Text>
-      <StatusBar style="auto" />
+    <View
+      style={{
+        justifyContent: "center",
+        alignItems: "center",
+        flex: 1,
+        gap: 10,
+      }}
+    >
+      <Text>{count}</Text>
+      <TouchableOpacity
+        style={{
+          height: 40,
+          width: 200,
+          alignItems: "center",
+          justifyContent: "center",
+          borderRadius: 14,
+          backgroundColor: "red",
+        }}
+        onPress={() => {
+          setCount(count + 1);
+        }}
+      >
+        <Text style={{ color: "white" }}>Кнопка</Text>
+      </TouchableOpacity>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
