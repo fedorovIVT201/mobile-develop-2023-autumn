06a1b9d49297e6aa3637b32d9c03f6c4020aedf1
layemee11 <struchkov.0234@gmail.com>
2023-09-28 19:55:53 +0900

    Revert "lab1 & lab2"
    This reverts commit deffff166612f11f13f8fea3f9ac6fc419fa580e.

--- struchkovVasiliy/App.js
+++ struchkovVasiliy/App.js
@@ -1,39 +1,21 @@
-import React from "react";
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import { NavigationContainer } from "@react-navigation/native";
-import Lab1 from "./screens/lab1";
-import Lab2 from "./screens/lab2";
-
-const Tab = createBottomTabNavigator();
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <Tab.Navigator
-      screenOptions={({ route }) => ({
-      tabBarActiveTintColor: "#007AFF",
-      tabBarInactiveTintColor: "#888888",
-      tabBarLabelStyle: {
-        fontSize: 16,
-        fontWeight: "bold",
-      },
-    })}
->
-        <Tab.Screen
-          name="Lab1"
-          component={Lab1}
-          options={{
-            tabBarLabel: "Лаб 1",
-          }}
-        />
-        <Tab.Screen
-          name="Lab2"
-          component={Lab2}
-          options={{
-            tabBarLabel: "Лаб 2",
-          }}
-        />
-      </Tab.Navigator>
-    </NavigationContainer>
+    <View style={styles.container}>
+      <Text>struchkovVasiliy</Text>
+      <Text>IVT-20-1</Text>
+      <StatusBar style="auto" />
+    </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
