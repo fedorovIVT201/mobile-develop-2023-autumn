3d79529d951f8a2e756da1a8f565782bb19f3c8f
EfimovAlexey <azerite2002@gmail.com>
2023-09-30 00:34:56 +0900

    Lab1

--- EfimovAlexey/App.js
+++ EfimovAlexey/App.js
@@ -1,10 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { useState } from 'react';
 
 export default function App() {
+  const [count, setCount] = useState(0)
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View style={[styles.container]}>
+      <Text style={{fontSize:50, fontFamily: 'Verdana'}}>{count}</Text>
+
+      <View style={styles.buttonsContainer}>
+          <TouchableOpacity style={styles.buttonGray} onPress={() => setCount(count - 5)}>
+              <Text style={{fontSize:25, fontFamily: 'Verdana'}}>-5</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity style={styles.buttonGray} onPress={() => setCount(count + 5)}>
+              <Text style={{fontSize:25, fontFamily: 'Verdana'}}>+5</Text>
+          </TouchableOpacity>
+      </View>
+      
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +26,24 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'lightblue',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  buttonsContainer: {
+    marginTop: 20,
+    display: 'flex',
+    flexDirection: 'row'
+  },
+  buttonGray: {
+    margin: 20,
+    height:50, 
+    borderRadius:10,
+    width:100,
+    alignItems:'center',
+    backgroundColor:'gray',
+    justifyContent:'center',
+    backgroundColor: 'orange'
+  },
 });
+
