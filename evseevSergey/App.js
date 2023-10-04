dfe4fc75df7c21c86bd780f11cde7b1dc6b32292
djwbhr <72724714+djwbhr@users.noreply.github.com>
2023-10-04 17:48:01 +0900

    Revert "firstLab"

--- evseevSergey/App.js
+++ evseevSergey/App.js
@@ -1,15 +1,20 @@
-import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
-import Lab1 from './screens/Lab1.js';
-const Tab = createBottomTabNavigator();
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
 
-  export default function App(){
+export default function App() {
   return (
-   <NavigationContainer>
-      <Tab.Navigator>
-        <Tab.Screen name="Lab1" component={Lab1} />
-      </Tab.Navigator>
-   </NavigationContainer>
+    <View style={styles.container}>
+      <Text>O BOJE MOY NAKONEC TO YA POSTAVIL ETOT REACT!</Text>
+      <StatusBar style="auto" />
+    </View>
   );
-};
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
