5bb11d4be8eda0b6be306d8ec5863c99c8473932
IIGGSS <tvorchh@mail.ru>
2023-10-05 20:22:34 +0900

    --idea

--- ivanovGavril/App.js
+++ ivanovGavril/App.js
@@ -2,7 +2,6 @@ import {NavigationContainer} from "@react-navigation/native";
 import CreateBottomTabNavigator from "@react-navigation/bottom-tabs/src/navigators/createBottomTabNavigator";
 import Lab1 from "./screens/lab1";
 import Lab2 from "./screens/lab2";
-import Lab3 from "./screens/lab3";
 
 export default function App() {
     const Tab = CreateBottomTabNavigator()
@@ -11,7 +10,6 @@ export default function App() {
             <Tab.Navigator>
                 <Tab.Screen name={"lab1"} component={Lab1}></Tab.Screen>
                 <Tab.Screen name={"lab2"} component={Lab2}></Tab.Screen>
-                <Tab.Screen name={"lab3"} component={Lab3}></Tab.Screen>
             </Tab.Navigator>
         </NavigationContainer>
     );
