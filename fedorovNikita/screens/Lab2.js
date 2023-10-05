8d01ca244a1e9dc81969ea1f0e1f205b1394dbc4
nikfed280199 <fednik1999@gmail.com>
2023-10-05 09:43:41 +0900

    feat: lab2 finalized

--- fedorovNikita/screens/Lab2.js
+++ fedorovNikita/screens/Lab2.js
@@ -1,39 +1,58 @@
-import { Text, TouchableOpacity, View } from "react-native";
+import { View, Text, Image, StyleSheet } from "react-native";
 import React, { useState, useEffect } from "react";
-import Button from "../components/Button";
-
-function useNow(refreshFrequency) {
-    const [now, setNow] = useState(new Date);
-    useEffect(() => {
-        const interval = setInterval(
-            () => setNow(new Date()),
-            refreshFrequency,
-        );
-        return () => clearInterval(interval);
-    });
-    return now;
-}
 
 const Lab2 = () => {
-    const dateState = useNow();
-    return (
-        <View
-          style={{
-            justifyContent: "center",
-            alignItems: "center",
-            flex: 1,
-            gap: 10,
-          }}
-        >
-          <Text>{dateState}</Text>
-          <Button
-            onPress={() => {
-                useNow();
-            }}
-            title={"Button"}
-          />
-        </View>
-    );
+  const [dateState, setDateState] = useState(new Date());
+  useEffect(() => {
+    setInterval(() => setDateState(new Date()), 30000);
+  }, []);
+  return (
+    <View style={styles.container}>
+      <Image
+        source={require("../icons/calendar.png")}
+        style={styles.image}
+      />
+      <Text>
+        {' '}
+        {dateState.toLocaleDateString('en-GB', {
+          day: 'numeric',
+          month: 'short',
+          year: 'numeric',
+        })}
+      </Text>
+      <View style={styles.space}></View>
+      <Image
+        source={require("../icons/clock.png")}
+        style={styles.image}
+      />
+      <Text>
+        {dateState.toLocaleString('en-US', {
+          hour: 'numeric',
+          minute: 'numeric',
+          hour12: true,
+        })}
+      </Text>
+    </View>
+  );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    backgroundColor: '#ecf0f1',
+    padding: 8,
+    flexDirection:'row',
+    alignItems:'center'
+  },
+  image: {
+    height: 30,
+    width: 30,
+  },
+  space: {
+    width: 20,
+    height: 20,
+  },
+});
 
 export default Lab2;
