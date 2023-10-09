ddcaf88c8f1f27ccdb6e715f2a6ed91e51b53c0e
djwbhr <sergey.evseev62@gmail.com>
2023-10-09 16:13:58 +0900

    fCommit

--- evseevSergey/App.js
+++ evseevSergey/App.js
@@ -1,17 +1,19 @@
-import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
-import Lab1 from './screens/Lab1.js';
-import Lab2 from './screens/Lab2.js';
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import Lab1 from "./screens/Lab1.js";
+import Lab2 from "./screens/Lab2.js";
+import Lab3 from "./screens/Lab3.js";
 const Tab = createBottomTabNavigator();
 
-  export default function App(){
+export default function App() {
   return (
-   <NavigationContainer>
+    <NavigationContainer>
       <Tab.Navigator>
-        <Tab.Screen name="Lab1" component={Lab1}/>
-        <Tab.Screen name="Lab2" component={Lab2}/>
+        <Tab.Screen name="Lab1" component={Lab1} />
+        <Tab.Screen name="Lab2" component={Lab2} />
+        <Tab.Screen name="Lab3" component={Lab3} />
       </Tab.Navigator>
-   </NavigationContainer>
+    </NavigationContainer>
   );
-};
+}
---
+++ evseevSergey/screens/Lab3.js
@@ -0,0 +1,10 @@
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, SafeAreaView, View, Platform } from "react-native";
+
+const Lab3 = () => {
+  return <SafeAreaView style={styles.mainBlock}></SafeAreaView>;
+};
+export default Lab3;
+
+});
