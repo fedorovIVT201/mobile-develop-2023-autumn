f04826c3efb8dfcf99747b04f12f21922c8e04a7
djwbhr <sergey.evseev62@gmail.com>
2023-10-19 11:11:03 +0900

    secondLabWPrettier

--- evseevSergey/screens/Lab2.js
+++ evseevSergey/screens/Lab2.js
@@ -1,48 +1,47 @@
-import React, {useState} from "react";
-import { StyleSheet, View, FlatList, Text} from "react-native";
+import React, { useState } from "react";
+import { StyleSheet, View, FlatList, Text } from "react-native";
 import Header from "../components/Header";
 import ListItem from "../components/ListItem";
 import Form from "../components/Form";
 
-const Lab2 = () =>{
-    const [listOfItems, setlistOfItems] = useState([
-        {text: "Купить молоко", key: "1"},
-        {text: "Помыть машину", key: "2"},
-        {text: "Почистить картошку", key: "3"},
-        {text: "Сходить в магазин", key: "4"},
-        
-    ])
+const Lab2 = () => {
+  const [listOfItems, setlistOfItems] = useState([
+    { text: "Купить молоко", key: "1" },
+    { text: "Помыть машину", key: "2" },
+    { text: "Почистить картошку", key: "3" },
+    { text: "Сходить в магазин", key: "4" },
+  ]);
 
-    const addHandler = (text) => {
-        setlistOfItems((list) => {
-            return [
-                {text: text, key: Math.random().toString(36).substring(7)},
-                ...list
-            ]
-        })
-    }
-    
-    const deleteHandler = (key) => {
-        setlistOfItems((list) => {
-            return list.filter(listOfItems => listOfItems.key != key )
-        })
-    }
+  const addHandler = (text) => {
+    setlistOfItems((list) => {
+      return [
+        { text: text, key: Math.random().toString(36).substring(7) },
+        ...list,
+      ];
+    });
+  };
 
-    return (
-        <View>
-            <Header/>
-            <Form addHandler={addHandler} />
-            <View>
-                <FlatList data={listOfItems} renderItem={({item})=>(
-                    <ListItem el = {item} deleteHandler={deleteHandler}/>
-                )} />
-            </View>
-        </View>
-    )
-}
+  const deleteHandler = (key) => {
+    setlistOfItems((list) => {
+      return list.filter((listOfItems) => listOfItems.key != key);
+    });
+  };
+
+  return (
+    <View>
+      <Header />
+      <Form addHandler={addHandler} />
+      <View>
+        <FlatList
+          data={listOfItems}
+          renderItem={({ item }) => (
+            <ListItem el={item} deleteHandler={deleteHandler} />
+          )}
+        />
+      </View>
+    </View>
+  );
+};
 export default Lab2;
 
-const styles = StyleSheet.create({
-
-
-})
+const styles = StyleSheet.create({});
