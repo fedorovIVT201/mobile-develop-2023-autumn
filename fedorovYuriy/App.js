39224a8e7a836dc13e52f8a1e7800b33143ee444
fedorovIVT201 <72394670+fedorovIVT201@users.noreply.github.com>
2023-11-15 20:59:23 +0900

    lab3

--- fedorovYuriy/App.js
+++ fedorovYuriy/App.js
@@ -1,38 +1,56 @@
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { NavigationContainer } from "@react-navigation/native";
-import { StyleSheet, Text, View, Image } from 'react-native';
+import { StyleSheet, Text, View, Image } from "react-native";
 import Lab1 from "./screens/Lab1";
 import Lab2 from "./screens/Lab2";
-
+import Lab3 from "./screens/Lab3";
 const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
       <Tab.Navigator>
-        <Tab.Screen name="Lab1" component={Lab1} 
-        options={{
-          tabBarIcon: ({ focused }) => (
-            <View>
-              <Image
-                source={require("./icons/Calculator_512.png")}
-                style={styles.img}
-              />
-            </View>
-          ),
-        }}
+        <Tab.Screen
+          name="Lab1"
+          component={Lab1}
+          options={{
+            tabBarIcon: ({ focused }) => (
+              <View>
+                <Image
+                  source={require("./icons/Calculator_512.png")}
+                  style={styles.img}
+                />
+              </View>
+            ),
+          }}
         />
-        <Tab.Screen name="Lab2" component={Lab2} 
-        options={{
-          tabBarIcon: ({ focused }) => (
-            <View>
-              <Image
-                source={require("./icons/fact.png")}
-                style={styles.img}
-              />
-            </View>
-          ),
-        }}
+        <Tab.Screen
+          name="Lab2"
+          component={Lab2}
+          options={{
+            tabBarIcon: ({ focused }) => (
+              <View>
+                <Image
+                  source={require("./icons/fact.png")}
+                  style={styles.img}
+                />
+              </View>
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="Lab3"
+          component={Lab3}
+          options={{
+            tabBarIcon: ({ focused }) => (
+              <View>
+                <Image
+                  source={require("./icons/todo.png")}
+                  style={styles.img}
+                />
+              </View>
+            ),
+          }}
         />
       </Tab.Navigator>
     </NavigationContainer>
