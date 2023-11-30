1dccd9402a516b40715717b0fa2c949eea018feb
flytkins <flytkins@gmail.com>
2023-11-30 20:04:39 +0900

    prettier

--- lytkinFedor/labs/lab2.tsx
+++ lytkinFedor/labs/lab2.tsx
@@ -1,69 +1,72 @@
-import {Text, View, Linking, Button, StyleSheet} from "react-native";
+import { Text, View, Linking, Button, StyleSheet } from "react-native";
 import { TouchableOpacity, ActivityIndicator } from "react-native";
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 
-const src = 'https://content.guardianapis.com/search?page=2&q=debate&api-key=test';
+const src =
+  "https://content.guardianapis.com/search?page=2&q=debate&api-key=test";
 const Lab2 = () => {
+  const [articles, setArticles] = useState([]);
+  const [delId, setDelId] = useState(0);
 
-    const [articles, setArticles] = useState([]);
-    const [delId, setDelId] = useState(0);
+  useEffect(() => {
+    const updatedArray = articles.filter((article) => {
+      return article.id !== delId;
+    });
 
-    useEffect(()=>{
-        const updatedArray = articles.filter((article) => {
-            return article.id !== delId;
-        });
+    setArticles(updatedArray);
+    console.log("update");
+  }, [delId]);
 
-        setArticles(updatedArray);
-        console.log('update')
-    }, [delId])
+  const update = () => {
+    axios.get(src).then((data) => {
+      setArticles(data.data.response.results);
+    });
+  };
 
-    const update = () => {
-        axios
-            .get(src)
-            .then(data => {
-                setArticles(data.data.response.results);
-            })
-    }
+  useEffect(() => {
+    update();
+  }, []);
 
-    useEffect(() => {
-        update()
-    }, [])
-
-    return (
-        <View
+  return (
+    <View
+      style={{
+        padding: 14,
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        gap: 10,
+      }}
+    >
+      <Text>TheGuardian articles</Text>
+      {articles.map((article) => {
+        return (
+          <View
             style={{
-                padding: 14,
-                flex: 1,
-                justifyContent: "center",
-                alignItems: "center",
-                gap: 10,
+              flex: 1,
+              alignItems: "center",
+              justifyContent: "space-between",
             }}
-        >
-            <Text>TheGuardian articles</Text>
-            {articles.map(article => {
-                return (
-                    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'space-between' }}>
-                        <Text style={styles.articles} onPress={() =>
-                            Linking.openURL(article.webUrl)}>{article.webTitle
-                        }</Text>
-                        <Text onPress={()=>
-                            setDelId(article.id)
-                        }>(x)</Text>
-                    </View>
-                )
-            })}
-            <Button title='Update' onPress={update}></Button>
-        </View>
-    );
+          >
+            <Text
+              style={styles.articles}
+              onPress={() => Linking.openURL(article.webUrl)}
+            >
+              {article.webTitle}
+            </Text>
+            <Text onPress={() => setDelId(article.id)}>(x)</Text>
+          </View>
+        );
+      })}
+      <Button title="Update" onPress={update}></Button>
+    </View>
+  );
 };
 
-
 const styles = StyleSheet.create({
-    articles: {
-        textAlign: "left",
-    }
-})
-
+  articles: {
+    textAlign: "left",
+  },
+});
 
 export default Lab2;
