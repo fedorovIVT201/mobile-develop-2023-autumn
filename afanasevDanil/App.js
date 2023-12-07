3f25441bdcacfdaf42dd71ac34fe738b31ad5624
FastMod <73865272+FastMod@users.noreply.github.com>
2023-12-07 20:15:17 +0900

    LAB 2

--- afanasevDanil/App.js
+++ afanasevDanil/App.js
@@ -1,20 +1,21 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'; 
+import Lab1 from "./screens/Lab1";
+import Lab2 from "./screens/Lab2";
+import Lab3 from "./screens/Lab3";
 
-export default function App() {
+const Tab = createBottomTabNavigator();
+export default function App () {
+
   return (
-    <View style={styles.container}>
-      <Text>Hello WORLD!</Text>
-      <StatusBar style="auto" />
-    </View>
+      <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen name="First lab" component={Lab1} />
+        <Tab.Screen name="Second lab" component={Lab2} />
+        <Tab.Screen name="Third lab" component={Lab3} />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
-}
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+}
