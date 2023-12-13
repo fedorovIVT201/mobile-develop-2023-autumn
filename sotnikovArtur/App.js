6f25503ceeae79fa8dde5283f22c82ee422e0375
Ruzeon <72289955+Ruzeon@users.noreply.github.com>
2023-12-13 21:47:02 +0900

    Laba 3

--- sotnikovArtur/App.js
+++ sotnikovArtur/App.js
@@ -2,6 +2,7 @@ import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { NavigationContainer } from "@react-navigation/native";
 import Lab1 from "./screens/Lab1";
 import Lab2 from "./screens/Lab2";
+import Lab3 from "./screens/Lab3";
 
 const Tab = createBottomTabNavigator();
 
@@ -11,6 +12,7 @@ export default function App() {
       <Tab.Navigator>
         <Tab.Screen name="Lab1" component={Lab1} />
         <Tab.Screen name="Lab2" component={Lab2} />
+        <Tab.Screen name="Lab3" component={Lab3} />
       </Tab.Navigator>
     </NavigationContainer>
   );
