c22d05fcf81e3e036dd7055736db953b7452eaca
EdgardKsenofontov <ksenofontov.edgard@gmail.com>
2023-12-21 06:11:34 +0900

    added Auth via firebase

--- ksenofontovEdgard/App.js
+++ ksenofontovEdgard/App.js
@@ -1,19 +1,14 @@
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { NavigationContainer } from "@react-navigation/native";
-import Lab1 from "./screens/Lab1";
-import Lab2 from "./screens/Lab2";
-import Lab3 from "./screens/Lab3"
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
-const Tab = createBottomTabNavigator();
+import MainStackNavigation from "./navigation/mainNavigation";
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Tab.Navigator>
-        <Tab.Screen name="Lab1" component={Lab1} />
-        <Tab.Screen name="Lab2" component={Lab2} />
-        <Tab.Screen name="Lab3" component={Lab3} />
-      </Tab.Navigator>
+      <MainStackNavigation />
     </NavigationContainer>
   );
 }
