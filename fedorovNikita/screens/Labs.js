197e5e8e9790addf8f8551bd2be26f497b002f0c
maz1no <fednik1999@gmail.com>
2023-12-28 20:22:19 +0900

    feat: lab5

--- fedorovNikita/screens/Labs.js
+++ fedorovNikita/screens/Labs.js
@@ -4,6 +4,7 @@ import Lab1 from "./Lab1";
 import Lab2 from "./Lab2";
 import Lab3 from "./Lab3";
 import Lab4 from "./Lab4";
+import Lab5 from "./Lab5";
 
 const Tab = createBottomTabNavigator();
 
@@ -14,6 +15,7 @@ const Labs = () => {
       <Tab.Screen name="Lab2" component={Lab2} />
       <Tab.Screen name="Lab3" component={Lab3} />
       <Tab.Screen name="Lab4" component={Lab4} />
+      <Tab.Screen name="Lab5" component={Lab5} />
     </Tab.Navigator>
   );
 }
