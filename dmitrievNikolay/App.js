c444c286c15b800311cdf28efb4f1fd7b24ec300
shutka007 <113580732+shutka007@users.noreply.github.com>
2023-12-28 12:52:25 +0900

    ReduxToDoList

--- dmitrievNikolay/App.js
+++ dmitrievNikolay/App.js
@@ -4,7 +4,6 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import AuthNav from './Navigations/AuthNav';
 import LabsNav from './Navigations/LabsNav';
-
 const Stack = createNativeStackNavigator();
 
 export default function App() {
--- dmitrievNikolay/Navigations/LabsNav.js
+++ dmitrievNikolay/Navigations/LabsNav.js
@@ -1,16 +1,22 @@
 import Lab1 from "../screens/Lab1";
 import Lab2 from "../screens/Lab2";
 import Lab3 from "../screens/Lab3";
+import Lab5 from "../screens/Lab5";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { Provider } from 'react-redux';
+import store from '../store';
 const Tab = createBottomTabNavigator();
 
 const LabsNav = () => {
   return (
+    <Provider store={store}>
     <Tab.Navigator>
       <Tab.Screen name="Lab1" component={Lab1} />
       <Tab.Screen name="Lab2" component={Lab2} />
       <Tab.Screen name="Lab3" component={Lab3} />
+      <Tab.Screen name="Lab5" component={Lab5} />
     </Tab.Navigator>
+    </Provider>
   );
 };
 
