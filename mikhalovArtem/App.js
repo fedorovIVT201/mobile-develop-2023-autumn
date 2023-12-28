857d479e3f90d1e30e8e0bcc995b4a7e39304037
Vasilisk9812 <scorpion73054@gmail.com>
2023-12-28 20:54:46 +0900

    2 lab

--- mikhalovArtem/App.js
+++ mikhalovArtem/App.js
@@ -1,22 +1,52 @@
-import React, { useState } from 'react';
-import { View, Text, Button, StyleSheet } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { View, Text, TextInput, Button, FlatList, StyleSheet } from 'react-native';
 
-const Counter = () => {
-  const [count, setCount] = useState(0);
+const TodoList = () => {
+  const [todoInput, setTodoInput] = useState('');
+  const [todos, setTodos] = useState([]);
 
-  const increment = () => {
-    setCount(count + 1);
+  const addTodo = () => {
+    if (todoInput === '') return;
+
+    setTodos([...todos, todoInput]);
+    setTodoInput('');
   };
 
-  const decrement = () => {
-    setCount(count - 1);
+  const deleteTodo = (index) => {
+    const newTodos = [...todos];
+    newTodos.splice(index, 1);
+    setTodos(newTodos);
   };
+
+  useEffect(() => {
+    // Этот эффект будет вызываться каждый раз, когда происходит изменение в состоянии todos
+    console.log('todos changed:', todos);
+  }, [todos]);
 
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Счетчик: {count}</Text>
-      <Button title="+" onPress={increment} />
-      <Button title="-" onPress={decrement} />
+      <Text style={styles.title}>To-Do List</Text>
+      
+      <View style={styles.inputContainer}>
+        <TextInput
+          style={styles.input}
+          placeholder="Введите задачу"
+          value={todoInput}
+          onChangeText={(text) => setTodoInput(text)}
+        />
+        <Button title="Добавить" onPress={addTodo} />
+      </View>
+
+      <FlatList
+        data={todos}
+        renderItem={({ item, index }) => (
+          <View style={styles.todoContainer}>
+            <Text style={styles.todoText}>{item}</Text>
+            <Button title="Удалить" onPress={() => deleteTodo(index)} />
+          </View>
+        )}
+        keyExtractor={(item, index) => index.toString()}
+      />
     </View>
   );
 };
@@ -24,13 +54,32 @@ const Counter = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+    padding: 20,
   },
-  text: {
+  title: {
     fontSize: 24,
+    fontWeight: 'bold',
     marginBottom: 20,
+  },
+  inputContainer: {
+    flexDirection: 'row',
+  },
+  input: {
+    flex: 1,
+    height: 40,
+    borderWidth: 1,
+    marginRight: 10,
+    paddingHorizontal: 10,
+  },
+  todoContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginBottom: 10,
+  },
+  todoText: {
+    flex: 1,
+    fontSize: 16,
   },
 });
 
-export default Counter;
+export default TodoList;
