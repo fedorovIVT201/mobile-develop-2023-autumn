6ae452638b7c6115ecdee9ecd4901cc47591cc56
Vladimir4123 <vladimirbn24@gmail.com>
2023-12-28 19:43:49 +0900

    Lab 1 + navigation

--- bochkarevVladimir/screens/Lab1.js
+++ bochkarevVladimir/screens/Lab1.js
@@ -1,47 +1,67 @@
-import {useState} from "react"
-import {TouchableOpacity, Text, View } from "react-native";
+import { Text, TouchableOpacity, View, StyleSheet } from "react-native";
+import { useState } from "react";
+import Button from "../components/Button";
 
-export default function App() {
-  const [count, setCount] = useState(0);
+export default function Lab1() {
+  const [count, setCount] = useState(1);
+
   return (
-    <View 
-    style={{
-      flex: 1,
-      gap: 10,
-      alignItems: "center",
-      justifyContent: "center",
-      }}>
-      <Text>{count}</Text>
-      <TouchableOpacity
-        style={{
-          height: 40,
-          width: 200,
-          alignItems: "center",
-          justifyContent: "center",
-          borderRadius: 14,
-          backgroundColor: "black",
-        }}
-        onPress={() => {
-          setCount(count + 1);
-        }}
-      >
-        <Text style={{ color: "white" }}>+</Text>
-      </TouchableOpacity>
-      <TouchableOpacity
-        style={{
-          height: 40,
-          width: 200,
-          alignItems: "center",
-          justifyContent: "center",
-          borderRadius: 14,
-          backgroundColor: "black",
-        }}
-        onPress={() => {
-          setCount(count - 1);
-        }}
-      >
-        <Text style={{ color: "white" }}>-</Text>
-      </TouchableOpacity>
+    <View style={styles.container}>
+      <View style={styles.whitecontainer}>
+        <Text style={styles.text}>{count}</Text>
+        <Button
+          onPress={() => {
+            setCount(count + 1);
+          }}
+          title={"+ 1"}
+        />
+        <Button
+          onPress={() => {
+            setCount(count - 1);
+          }}
+          title={"- 1"}
+        />
+        <Button
+          onPress={() => {
+            setCount(count ** 2);
+          }}
+          title={"** 2"}
+        />
+        <Button
+          onPress={() => {
+            setCount(count / 2);
+          }}
+          title={"/ 2"}
+        />
+        <Button
+          onPress={() => {
+            setCount(count * 0);
+          }}
+          title={"Обновить"}
+        />
+      </View>
     </View>
   );
 }
+const styles = StyleSheet.create({
+  container: {
+    justifyContent: "center",
+    alignItems: "center",
+    flex: 1,
+    gap: 10,
+    backgroundColor: "rgb(200, 200, 140)",
+  },
+  text: {
+    marginTop: 10,
+    marginBottom: 10,
+    color: "black",
+    fontSize: 32,
+  },
+  whitecontainer: {
+    width: 296,
+    height: 330,
+    alignItems: "center",
+    backgroundColor: "rgba(170, 180, 140, 1)",
+    borderRadius: 40,
+  },
+});
