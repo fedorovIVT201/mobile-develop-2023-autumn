97d5831c3e516666ca09e5ae9dcb69678ade8a56
IIGGSS <tvorchh@mail.ru>
2023-12-28 20:00:25 +0900

    lab4

--- ivanovGavril/screens/lab2.js
+++ ivanovGavril/screens/lab2.js
@@ -1,19 +1,29 @@
-import {Alert, StyleSheet, Text, View} from 'react-native';
-import {useEffect, useState} from "react";
+import { Alert, StyleSheet, Text, View } from "react-native";
+import { useEffect, useState } from "react";
 
 export default function Lab2() {
-    const createTwoButtonAlert = () => Alert.alert('useEffect', 'This is Alert called by useEffect', [{
-        text: 'hello', onPress: () => console.log('Cancel Pressed'),
-    }, {text: 'world', onPress: () => console.log('OK Pressed')},]);
-    useEffect(() => {
-        createTwoButtonAlert();
-    }, [])
-    return (<View style={styles.container}>
-        <Text>useEffect</Text>
-    </View>);
+  const createTwoButtonAlert = () =>
+    Alert.alert("useEffect", "This is Alert called by useEffect", [
+      {
+        text: "hello",
+        onPress: () => console.log("Cancel Pressed"),
+      },
+      { text: "world", onPress: () => console.log("OK Pressed") },
+    ]);
+  useEffect(() => {
+    createTwoButtonAlert();
+  }, []);
+  return (
+    <View style={styles.container}>
+      <Text>useEffect</Text>
+    </View>
+  );
 }
 const styles = StyleSheet.create({
-    container: {
-        flex: 1, backgroundColor: '#fff', alignItems: 'center', justifyContent: 'center',
-    },
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
 });
---
+++ ivanovGavril/screens/signOut.js
@@ -0,0 +1,17 @@
+import { auth } from "./firebase";
+
+const SignOut = ({ navigation }) => {
+  const handleSignOut = () => {
+    auth
+      .signOut()
+      .then(() => {
+        navigation.replace("Login");
+      })
+      .catch((error) => {
+        alert(error.message);
+      });
+  };
+  handleSignOut();
+  return;
+};
+export default SignOut;
